f4ca320339e668d60ad1106143775311780d73f3
ashishb888 <ashish.bhosle008@gmail.com>
2020-09-14 16:02:43 +0530

    api-provider: Initial commit

---
+++ api-provider/src/main/java/poc/springcloud/ApiProviderApplication.java
@@ -0,0 +1,13 @@
+package poc.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProviderApplication.class, args);
+	}
+
+}
