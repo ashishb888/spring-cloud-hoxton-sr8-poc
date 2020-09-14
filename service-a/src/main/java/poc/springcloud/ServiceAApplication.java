dd41f72b6bb6e8c07431bf87471877dac59420f8
ashishb888 <ashish.bhosle008@gmail.com>
2020-09-14 14:16:31 +0530

    service-a: Added an endpoint to list all instances

--- service-a/src/main/java/poc/springcloud/ServiceAApplication.java
+++ service-a/src/main/java/poc/springcloud/ServiceAApplication.java
@@ -1,7 +1,14 @@
 package poc.springcloud;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ServiceAApplication {
@@ -10,4 +17,16 @@ public class ServiceAApplication {
 		SpringApplication.run(ServiceAApplication.class, args);
 	}
 
+}
+
+@RestController
+class ServiceARestController {
+
+	@Autowired
+	private DiscoveryClient dc;
+
+	@GetMapping("/instances")
+	List<?> instances() {
+		return dc.getServices().stream().map(serviceId -> dc.getInstances(serviceId)).collect(Collectors.toList());
+	}
 }
