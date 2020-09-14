38a031606912c85f911b6187f666a9ed82e6156f
ashishb888 <ashish.bhosle008@gmail.com>
2020-09-14 16:11:46 +0530

    api-provider: Added an endpoint random-number

--- api-provider/src/main/java/poc/springcloud/ApiProviderApplication.java
+++ api-provider/src/main/java/poc/springcloud/ApiProviderApplication.java
@@ -1,7 +1,15 @@
 package poc.springcloud;
 
+import java.time.Duration;
+import java.util.Random;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class ApiProviderApplication {
@@ -10,4 +18,15 @@ public class ApiProviderApplication {
 		SpringApplication.run(ApiProviderApplication.class, args);
 	}
 
+}
+
+@RestController
+class RandomNumberRestController {
+
+	Random random = new Random();
+
+	@GetMapping(path = "/random-number", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	Flux<Integer> randomNumber() {
+		return Flux.interval(Duration.ofSeconds(3)).map(i -> random.nextInt(100));
+	}
 }
