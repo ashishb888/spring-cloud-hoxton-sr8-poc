41db46f100e0d81335ac2aca9de8ce6c14d6cd44
ashishb888 <ashish.bhosle008@gmail.com>
2020-09-17 11:29:12 +0530

    api-gateway: Added routes for red-service & green service

--- api-gateway/src/main/java/poc/springcloud/ApiGatewayApplication.java
+++ api-gateway/src/main/java/poc/springcloud/ApiGatewayApplication.java
@@ -2,12 +2,21 @@ package poc.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder) {
+		return builder.routes().route(p -> p.path("/red-service").uri("lb://red-service"))
+				.route(p -> p.path("/green-service").uri("lb://green-service")).build();
 	}
 
 }
